043fd5b577095196b3e8f18defc04a18f5007e65
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2023-01-11 23:10:33 +0100

    Delete Desktop directory

--- Desktop/Animated button/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector("[data-btn]");
-btn.addEventListener('click', ()=>{
-    btn.classList.add('animating')
-})
-
-let x = document.querySelector('path')
-let y = x.getTotalLength()
-console.log(y)
